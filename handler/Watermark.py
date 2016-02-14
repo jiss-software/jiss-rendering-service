9b64b51007e27cb88ddacdd0cddc6be51946d4b5
Антон Ишков <hasado@yandex.ru>
2016-02-14 14:00:12 +0100

    Improve watermark functionality (4)

--- handler/Watermark.py
+++ handler/Watermark.py
@@ -12,10 +12,9 @@ class WatermarkHandler(core.BaseHandler):
         self.logger.info('Request watermark generation for remote file')
 
         name = '/tmp/%s.png' % str(uuid.uuid4())
-        proportion = int(self.request.headers.get('proportion', default=1.5))
-        text = self.request.headers.get('X-Jiss-Text', default='Test')
+        args = self._get_args()
 
-        add_watermark(open_remote_image(self.get_query_argument('url')), name, text, proportion)
+        add_watermark(open_remote_image(self.get_query_argument('url')), name, args['text'], args['proportion'])
         self.response_file(name)
 
     @tornado.web.asynchronous
@@ -23,13 +22,18 @@ class WatermarkHandler(core.BaseHandler):
     def post(self):
         self.logger.info('Request watermark generation for request file')
 
-        proportion = int(self.request.headers.get('proportion', default=1.5))
-        text = self.request.headers.get('X-Jiss-Text', default='Test')
+        args = self._get_args()
 
         for item in self.request.files.values():
             for file_info in item:
                 name = '/tmp/%s-%s.pdf' % (time.time(), file_info['filename'])
 
-                add_watermark(open_image(file_info['body']), name, text, proportion)
+                add_watermark(open_image(file_info['body']), name, args['text'], args['proportion'])
                 self.response_file(name)
                 return
+
+    def _get_args(self):
+        return {
+            'proportion': int(self.request.headers.get('proportion', default=1.5)),
+            'text': self.request.headers.get('X-Jiss-Text', default='Test')
+        }
