a3f825b2e8dc834b3fcc8d4c31f5e8458b4c73b1
Антон Ишков <hasado@yandex.ru>
2016-02-14 12:38:36 +0100

    Improve install

--- pre_install.sh
+++ pre_install.sh
@@ -1,3 +1,9 @@
-apt-get -y install python-pip
 apt-get build-dep python-imaging
 apt-get -y install libjpeg8 libjpeg62-dev libfreetype6 libfreetype6-dev fonts-liberation
+
+wget http://download.gna.org/wkhtmltopdf/0.12/0.12.1/wkhtmltox-0.12.1_linux-trusty-amd64.deb
+dpkg -i wkhtmltox-0.12.1_linux-trusty-amd64.deb
+
+cd /usr/local/bin
+cp wkhtmltoimage /usr/bin/wkhtmltoimage
+cp wkhtmltopdf /usr/bin/wkhtmltopdf
